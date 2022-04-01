c0be120a6f63f789d6bf6b1a1bcbb0b3fee40276
ikylios <ikylioss@gmail.com>
2022-04-01 14:24:56 +0300

    interactable new ghost_npc

--- src/Interactable.gd
+++ src/Interactable.gd
@@ -1,10 +1,20 @@
-extends Node2D
-
+extends Area2D
+ 
+var interactable: bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	connect("body_entered", self, "_on_Interaction_area_body_entered")
+	connect("body_exited", self, "_on_Interaction_area_body_exited")
 
+func _process(delta):
+	if Input.is_action_pressed("interact"):
+		interact()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func interact():
+	pass
+
+func _on_Interaction_area_body_entered(body):
+	interactable = true
+
+func _on_Interaction_area_body_exited(body):
+	interactable = false
