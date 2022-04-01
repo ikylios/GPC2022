cd6e5b0783fea3d8866922c4574393e97978a211
ikylios <ikylioss@gmail.com>
2022-04-01 14:51:38 +0300

    interactable ghost

--- src/scenes/characters/Ghost_NPC.gd
+++ src/scenes/characters/Ghost_NPC.gd
@@ -1,26 +1,24 @@
-extends KinematicBody2D
+extends "res://scenes/Interactable.gd"
 
 var order
-var interactable = false
-signal npc_area_entered
-signal npc_area_exited
-
 var order_list = ["spydäri", "ranskikset"]
 
 
 func _ready():
 	order = generate_order()
 	$AnimatedSprite.play("idle")
-
-	connect("npc_area_entered", get_parent(), "_on_NPC_area_entered")
-	connect("npc_area_exited", get_parent(), "_on_NPC_area_exited")
 	add_to_group("new_customers")
 
 
 func _process(_delta):
 	$AnimatedSprite.playing = interactable
+	
 
+# -------------- Interaction --------------
 
+func interact():
+	$Order_bubble.display_order(order)
+	
 	
 # -------------- Moving functionalities --------------
 
@@ -37,17 +35,3 @@ func generate_order():
 
 func get_order():
 	return order
-
-
-# -------------- Interaction --------------
-
-func interact():
-	$Order_bubble.display_order(order)
-
-func _on_Interaction_Area_body_entered(body):
-	emit_signal("npc_area_entered", self)
-	interactable = true
-
-func _on_Interaction_Area_body_exited(body):
-	emit_signal("npc_area_exited")
-	interactable = false
