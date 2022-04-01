ad8586471d2c9424839e628dfcbb0a39fb63981f
ikylios <ikylioss@gmail.com>
2022-04-01 15:19:43 +0300

    interactable ingredient box

--- src/scenes/Interactable.gd
+++ src/scenes/Interactable.gd
@@ -1,6 +1,10 @@
 extends Area2D
- 
+
 var interactable: bool = false
+
+func _ready():
+	connect("body_entered", self, "_on_Interaction_area_body_entered")
+	connect("body_exited", self, "_on_Interaction_area_body_exited")
 
 func _process(delta):
 	if Input.is_action_pressed("interact") and interactable:
@@ -10,6 +14,7 @@ func interact():
 	pass
 
 func _on_Interaction_area_body_entered(body):
+	print("interacting area of ", self)
 	interactable = true
 
 func _on_Interaction_area_body_exited(body):
