0af412da153e63f9ec0a4320e484135df7c5d2ac
ikylios <ikylioss@gmail.com>
2022-04-02 17:55:08 +0300

    customer is able to receive the ordered meal and refuse wrong meal. very ugly code

--- src/scenes/characters/Ghost_NPC.gd
+++ src/scenes/characters/Ghost_NPC.gd
@@ -2,6 +2,8 @@ extends "res://scenes/Interactable.gd"
 
 var order
 var order_list = ["spydäri", "ranskikset"]
+var received_order = null
+var received_order_sprite
 
 func _ready():
 	order = generate_order()
@@ -15,8 +17,24 @@ func _process(_delta):
 
 # -------------- Interaction --------------
 
+func correct_meal(meal):
+	return meal.name == order
+
+func waiting_on_meal():
+	return self in get_tree().get_nodes_in_group("customers")
+
 func interact():
-	$Order_bubble.display_order(order)
+	var player = get_parent().get_node("Player")
+	var meal = player.get_carried_item()
+	
+	if waiting_on_meal() and meal:
+		if correct_meal(meal):
+			player.drop_carried_item()
+			receive_meal(meal)
+		else:
+			print("wrong meal!")
+	elif !received_order:
+		$Order_bubble.display_order(order)
 	
 	
 # -------------- Moving functionalities --------------
@@ -34,3 +52,12 @@ func generate_order():
 
 func get_order():
 	return order
+
+func receive_meal(meal):
+	received_order = meal
+	print("received ", received_order)
+	
+	received_order_sprite = Sprite.new()
+	received_order_sprite.set_texture(load(meal.path))
+	add_child(received_order_sprite)
+	received_order_sprite.position = Vector2($AnimatedSprite.position.x, $AnimatedSprite.position.y + 7)
