2f0460f0a16a9a7c0fbd63df03289fabdf7e4f2d
sumuh <muhli.susanna@gmail.com>
2022-04-24 13:25:00 +0300

    Progress bar added for frying

--- src/scenes/furniture/Frying_pan.gd
+++ src/scenes/furniture/Frying_pan.gd
@@ -1,16 +1,34 @@
 extends "res://scenes/utils/Interactable.gd"
 
+var timer_value = 0
+var being_fried = null
+
 func interact():
 	var player = get_parent().get_node("Player")
 	var player_item = player.get_carried_item()
 	
 	if player_item:
-		var fried_item = get_node("/root/Main/Food_index").try_to_fry(player_item.name)
+		being_fried = get_node("/root/Main/Food_index").try_to_fry(player_item.name)
 		
-		if fried_item:
-			$AudioStreamPlayer.play()
-			player.drop_carried_item()
-			yield(get_tree().create_timer(2.0), "timeout")
-			player.set_carried_item(fried_item)
+		if being_fried:
+			start_frying(player)
 		else:
 			print("not a fryable ingredient!")
+
+func start_frying(player):
+	$AudioStreamPlayer.play()
+	player.drop_carried_item()
+	$ProgressBarControl.show()
+	$ProgressBarControl/FryTimer.start()
+
+func stop_frying():
+	var player = get_parent().get_node("Player")
+	$ProgressBarControl/FryTimer.stop()
+	$ProgressBarControl.hide()
+	$ProgressBarControl/TextureProgress.value=0
+	player.set_carried_item(being_fried)
+
+
+func _on_TextureProgress_value_changed(value):
+	if value == 100:
+		stop_frying()
